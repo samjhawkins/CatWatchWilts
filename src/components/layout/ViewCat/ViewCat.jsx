7c5b26c28838f110788c618aa82ea2fa57783a8c
samjhawkins <samjhawkins@outlook.com>
2020-05-25 19:28:52 +0100

    form - new stuff on the form side of things

--- src/components/layout/ViewCat/ViewCat.jsx
+++ src/components/layout/ViewCat/ViewCat.jsx
@@ -7,6 +7,7 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { withCatContext } from '../../common/wrappers/CatContext';
 import CatForm from '../../common/CatForm/CatForm';
+import getMUIDimensions from "../../../utils/getMUIDimensions";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -42,11 +43,12 @@ const useStyles = makeStyles((theme) => ({
 const ViewCat = ({ selectedCat }) => {
   console.log('selectedViewCat', selectedCat);
   const classes = useStyles({ selectedCat });
+  const breakpoints = getMUIDimensions(selectedCat.rows, selectedCat.cols);
 
   return (
     <Grid item container component="main" xs={10} className={classes.root}>
-      <Grid item sm={12} md={7} className={classes.image} />
-      <Grid item sm={12} md={5} component={Paper} elevation={6} square>
+      <Grid item {...breakpoints.image} className={classes.image} />
+      <Grid item {...breakpoints.div} component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Typography component="h1" variant="h5">
             {selectedCat.name}
@@ -62,6 +64,8 @@ ViewCat.propTypes = {
   selectedCat: PropTypes.shape({
     name: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
+    rows: PropTypes.number.isRequired,
+    cols: PropTypes.number.isRequired,
   }).isRequired,
 };
 
