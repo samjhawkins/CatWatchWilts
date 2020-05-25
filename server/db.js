b9802d31845cce43d50b80ac6363f0efeebb59d2
samjhawkins <samjhawkins@outlook.com>
2020-05-25 20:55:32 +0100

    db - add next of db

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const aws = require('aws-sdk');
 
-aws.config.update({ region: 'REGION' });
+aws.config.update({ region: 'eu-west-2' });
 const ddb = new aws.DynamoDB({ apiVersion: '2012-08-10' });
 const TableName = 'Cats';
 
@@ -17,8 +17,10 @@ module.exports = (app) => {
     const response = ddb.putItem(params, function (err, data) {
       if (err) {
         console.log('Error', err);
+        return err;
       } else {
         console.log('Success', data);
+        return data;
       }
     });
     res.send({ data: response });
@@ -26,12 +28,14 @@ module.exports = (app) => {
 
   // get list of all the people taking part
   app.get('/cats', function (req, res) {
+    console.log('get all');
     const cats = [{}];
     res.send({ data: cats });
   });
 
   // get specific cat details
   app.get('/cats/:id', function (req, res) {
+    console.log('get cat', req.params.id);
     const cat = { id: req.params.id };
     res.send({ data: cat });
   });
