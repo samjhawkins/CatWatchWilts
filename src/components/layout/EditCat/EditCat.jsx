7ce4a0ad083e9b98df31b70e61c65909e08ae6f5
Sam <samjhawkins@outlook.com>
2020-09-25 15:16:24 +0100

    authBackEnd - update editCat

--- src/components/layout/EditCat/EditCat.jsx
+++ src/components/layout/EditCat/EditCat.jsx
@@ -5,24 +5,30 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { withCatContext } from '../../common/wrappers/CatContext';
 import CatForm from '../../common/CatForm/CatForm';
-import getMUIDimensions from '../../../utils/getMUIDimensions';
 import { useStyles } from '../../../themes/useStyles';
 import { withMediaQuery } from '../../common/wrappers/MediaQuery';
 
 const EditCat = ({ selectedCat, matches: { aboveSM } }) => {
   const classes = useStyles({ imgUrl: selectedCat.image, aboveSM });
-  const breakpoints = getMUIDimensions(selectedCat.rows, selectedCat.cols);
+  const wider = selectedCat.cols / selectedCat.rows >= 1;
+
+  const xsRatio = wider || !aboveSM ? 12 : 6;
 
   return (
-    <Grid item container component="main" xs={10} className={classes.root}>
-      <Grid item {...breakpoints.image} className={classes.image} />
-      <Grid item {...breakpoints.div} component={Paper} elevation={6} square>
-        <div className={classes.flex_paper}>
-          <Typography component="h1" variant="h5">
-            {selectedCat.name}
-          </Typography>
-          <CatForm mode="view" cat={selectedCat} withInitialDisplay />
-        </div>
+    <Grid item container component="main" className={classes.root}>
+      <Grid
+        item
+        xs={xsRatio}
+        md={6}
+        component="img"
+        src={selectedCat.image}
+        className={classes.fullWidth}
+      />
+      <Grid item xs={xsRatio} md={6} component={Paper} elevation={6} square>
+        <Typography component="h1" variant="h5">
+          {selectedCat.name}
+        </Typography>
+        <CatForm mode="view" cat={selectedCat} />
       </Grid>
     </Grid>
   );
@@ -30,11 +36,19 @@ const EditCat = ({ selectedCat, matches: { aboveSM } }) => {
 
 EditCat.propTypes = {
   selectedCat: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    rows: PropTypes.number.isRequired,
-    cols: PropTypes.number.isRequired,
+    name: PropTypes.string,
+    image: PropTypes.string,
+    rows: PropTypes.number,
+    cols: PropTypes.number,
   }).isRequired,
+  matches: PropTypes.shape({
+    aboveSM: PropTypes.bool.isRequired,
+    aboveMD: PropTypes.bool.isRequired,
+  }),
+};
+
+EditCat.defaultProps = {
+  matches: { aboveSM: false, aboveMD: false },
 };
 
 export default withCatContext(withMediaQuery(EditCat));
