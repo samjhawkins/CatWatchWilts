f464abfdba2bc93ba0352532bc1cb1a33add1f7d
jacoblambrou <j.lambrou94@gmail.com>
2020-09-28 21:57:28 +0100

    authBackEnd - updating auth

--- server/token.js
+++ server/token.js
@@ -5,28 +5,35 @@ const app = express.Router();
 const redirectUri = 'https://localhost:40000';
 
 // Get all cats details
-app.get('/token', (req, res) => {
+app.get('/', (req, res, next) => {
   console.log('get token');
   const config = {
     headers: {
       Authorization: process.env.BASE64_SECRET,
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
   };
   console.log('process.env.64', process.env.BASE64_SECRET);
+  const params = new URLSearchParams();
+  params.append('code', req.params.code);
+  params.append('grant_type', 'authorization_code');
+  params.append('redirect_uri', redirectUri);
+  params.append('client_id', process.env.CLIENT_ID);
+  // params.append('scope', 'all');
   axios
-    .post(
-      'https://catwatch.auth.eu-west-2.amazoncognito.com/oauth2/token',
-      `code=${req.params.code}&grant_type=authorization_code&redirect_uri=${redirectUri}&client_id=${process.env.CLIENT_ID}&scope=all`,
-      config,
-    )
-    .then((data) => {
-      console.log('data', data);
-      // res.send({ token: data });
-    })
-    .catch((e) => {
-      console.log('Error translating:', e);
-      res.error(new Error('There was a problem verifying the code'));
-    });
+      .post(
+        'https://catwatch.auth.eu-west-2.amazoncognito.com/oauth2/token',
+        params,
+        config,
+      )
+      .then((data) => {
+        console.log('data', data);
+        // res.send({ token: data });
+      })
+      .catch((e) => {
+        console.log('Error translating:', e.message);
+        next(e);
+      });
 });
 
 module.exports = app;
